04f3fd7749fd3ef5e4bcf469d372a5ee483e4c18
HamInGyu <hfire1207@gmail.com>
2022-10-26 17:32:48 +0900

    백준 11651 좌표 정렬하기2

---
+++ problems/O_11651.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const [_, ...inputData] = fs.readFileSync(0).toString().trim().split("\n");
+
+const points = inputData.map((point) => point.split(" ").map(Number));
+
+points.sort((a, b) => {
+  if (a[1] === b[1]) {
+    return a[0] - b[0];
+  }
+  return a[1] - b[1];
+});
+
+console.log(points.map((point) => point.join(" ")).join("\n"));
