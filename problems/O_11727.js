fe318b1020142deee4c471c6053a3a77c640e465
HamInGyu <hfire1207@gmail.com>
2022-11-14 19:18:48 +0900

    백준 11727 2xn 타일링2

---
+++ problems/O_11727.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const N = Number(fs.readFileSync(0));
+
+const DP = new Array(N + 1).fill(0);
+
+DP[1] = 1;
+DP[2] = 3;
+
+for (let i = 3; i <= N; i++) {
+  DP[i] = (DP[i - 1] + DP[i - 2] * 2) % 10007;
+}
+
+console.log(DP[N]);
